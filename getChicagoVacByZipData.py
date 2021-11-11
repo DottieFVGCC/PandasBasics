7080d8d95663bd70c15023a8830f493f2e561b5e
DottieFVGCC <56316682+DottieFVGCC@users.noreply.github.com>
2021-11-11 22:11:57 +0000

    Initial commit

---
+++ getChicagoVacByZipData.py
@@ -0,0 +1,17 @@
+#Based on tutorial: using Pandas and Python to explore your dataset
+#https://realpython.com/pandas-python-explore-dataset/
+
+import requests  #a Python library for handling HTTP requests
+
+#to find  government datasets go to data.gov; type keywords in the search box; e.g. "Chicago COVID".  From the results you can filter by format to get csv files or JSON files. In the list of datasets, hover over the CSV button, right-click and copy link address which you willpaste into the download_url variable below
+
+# Chicago vaccinations by zip code
+download_url = "https://data.cityofchicago.org/api/views/553k-3xzc/rows.csv?accessType=DOWNLOAD"
+target_csv_path = "chicago_vac_by_zip.csv"
+
+response = requests.get(download_url)
+
+response.raise_for_status()    # Check that the request was successful
+with open(target_csv_path, "wb") as f:
+    f.write(response.content)
+print("Download ready.")
---
+++ main.py
@@ -0,0 +1,45 @@
+#Based on tutorial: using Pandas and Python to explore your dataset
+#https://realpython.com/pandas-python-explore-dataset/
+
+#before running this file, run the get...Data.py file to populate the dataset
+#HINT: you can change which file runs when the Run button is clicked - edit the .replit file
+
+import pandas as pd  #convention is to use pd as alias for pandas 
+
+# Read data from csv into a variable; the result is a Dataframe
+#df = pd.read_csv("nba_all_elo.csv")  #NBA data
+
+df = pd.read_csv("chicago_vac_by_zip.csv")  #Chicago vaccination data
+
+print(type(df))  #display the type of the df variable - it is a DataFrame!
+print(len(df))  #number of records in the dataset
+print(df.shape)  #Dimensionality: rows and columns
+
+pd.set_option("display.max.columns", None)  #show all columns
+pd.set_option("display.precision", 2)  #decimal places
+
+#head() and tail() functions get rows from the beginning and end of the dataset; they default
+# to 5 rows but can take a parameter for number of rows wanted
+print(df.head()) #the first 5 rows
+#print(df.head(10)) #the first 10 rows
+print(df.tail()) #the last 5 rows
+
+print("Dataframe Info:")
+print(df.info()) #describes the columns
+
+print("Dataframe Describe")
+print(df.describe()) #some basic statistics
+
+print("Dataframe Describe: Include Objects")
+print(df.describe(include=object)) #some basic statistics for objects
+
+#convert the dataframe to HTML and save to a file
+# in a future phase of this development we may create a Flask Web site and write to a template within the project
+html_table = df.to_html()
+html_file = open("index.html", "w")
+html_file.write(html_table)
+html_file.close()
+
+
+
+
